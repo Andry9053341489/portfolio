9d02489bdb549cb75fd07b0962521aff80c1a588
Andry9053341489 <reg9053341489@yandex.ru>
2020-11-23 17:31:07 +0400

    adaptive

--- js/main.js
+++ js/main.js
@@ -2,8 +2,15 @@ new WOW().init();
 
 var mySwiper = new Swiper('.swiper-container', {
 
+	autoplay: {
+		delay: 5000,
+	},
+
 	// If we need pagination
 	pagination: {
-		el: '.swiper-pagination',
+		el: '.project-pagination',
+		bulletClass: 'project-bullet',
+		bulletActiveClass: 'project-bullet-active',
+		clickable: true,
 	},
-})
+});
